927737db9657143713e171768d1349581c9e42ca
tsohleDev <mokhemisitsohle@gmail.com>
2022-12-24 04:19:39 +0200

    Express

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello, World!',
+    numbers: [1, 2, 3]
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
