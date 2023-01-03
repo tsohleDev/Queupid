788a7e42fef4b739e1221b1ba08ecc2be19e44b6
tsohleDev <mokhemisitsohle@gmail.com>
2023-01-03 02:22:10 +0200

    connect

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ const { connect } = require('http2');
 const { cli } = require('webpack-dev-server');
 var conString = "postgres://clients_user:PDEYxoHr8eQiipm7b8lgI9LrqkJZPZwP@dpg-cehoqlpgp3jvlf06e91g-a/clients" //Can be found in the Details page
 
+
+try {
+  await client.connect()
+} catch (error) {
+  console.log('couldnt connect to database', error.message);
+}
+
 let queue = []
 const seats = [
   {
