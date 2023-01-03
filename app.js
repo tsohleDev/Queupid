850c98ec94e7e4d68a19eb2d1cd8709232d30416
tsohleDev <mokhemisitsohle@gmail.com>
2023-01-03 03:13:25 +0200

    connect

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const path = require('path')
 
 var pg = require('pg');
 const { connect } = require('http2');
-const { cli } = require('webpack-dev-server');
-var conString = "postgres://clients_user:PDEYxoHr8eQiipm7b8lgI9LrqkJZPZwP@dpg-cehoqlpgp3jvlf06e91g-a/clients" //Can be found in the Details page
+var conString = "postgres://clients_user:PDEYxoHr8eQiipm7b8lgI9LrqkJZPZwP@dpg-cehoqlpgp3jvlf06e91g-a.singapore-postgres.render.com/clients" //Can be found in the Details page
  
 const connectToDatabase = async () => {
   try {
