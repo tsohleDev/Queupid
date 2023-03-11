5ca3f63c5a6cf2cfcf1a79d16e1b602174662796
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-11 08:34:23 +0200

    chang screen size

--- src/components/Header/Links/links.js
+++ src/components/Header/Links/links.js
@@ -1,21 +1,12 @@
 import { NavLink } from 'react-router-dom';
-import { writeHomeRoute } from '../../../redux/menutoogle/menutoogle';
-import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 function Links(props) {
   const { name, path } = props;
 
-  const {openMenu} = useSelector(state => state.menuToogle);
-  const dispatch = useDispatch();
-  
-  const handleClick = () => {
-      dispatch(writeHomeRoute(path));
-  }
   return (
     <NavLink
       to={path}
-      onClick={handleClick}
       className={({ isActive }) => (isActive ? 'active-link' : 'not-active-class')}
     >
       {name}
